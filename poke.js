8ffe70fed8c86c0b5c5f6e4b8bbf73e1a9a01ebc
CaroDuran <70724144+CaroDuran@users.noreply.github.com>
2020-10-19 16:29:09 -0300

    se modifica js

--- poke.js
+++ poke.js
@@ -16,9 +16,6 @@ $(function () {
             var introPokemon = respuesta.name;
 
             // Cartilla Pokémon: 
-            //console.log(respuesta.name); // Nombre Pokémon // bulbasaur
-            //console.log(respuesta.sprites.front_default); // Img sprite
-            //console.log(respuesta.weight); // Peso del Pokémon // 69
             $(".namepoke").text(introPokemon);
             $("#imgpoke").attr("src", "" + respuesta.sprites.front_default + "" );
             $(".pesopoke").text(` Peso: ${respuesta.weight} [kg]`);
@@ -26,21 +23,16 @@ $(function () {
 
             // Información del Grafico // 
             var velocidadPokemon = respuesta.stats[5].base_stat;
-            //console.log(base[5].stat.name); // velocidad 
             var especialDefensa = respuesta.stats[4].base_stat;
-            //console.log(base[4].stat.name); // defensa especial
             var especialAtaque = respuesta.stats[3].base_stat;
-            //console.log(base[3].stat.name); // ataque especial
             var defensaPokemon = respuesta.stats[2].base_stat;
-            //console.log(base[2].stat.name); // defensa
             var ataquePokemon = respuesta.stats[1].base_stat;
-            //console.log(base[1].stat.name); // ataque
             var vidaPokemon = respuesta.stats[0].base_stat;
-            //console.log(base[0].stat.name); // Vida del Pokémon // hp
+            
 
             var chart = new CanvasJS.Chart("chartContainer", {
                 theme: "dark2",  
-                animationEnabled: false, // change to true		
+                animationEnabled: false,	
                 title:{
                     text: "Stats Base"
                 },
@@ -52,7 +44,7 @@ $(function () {
                 },
                 data: [
                 {
-                    // Change type to "bar", "area", "spline", "pie",etc.
+                
                     type: "column",
                     dataPoints: [
                         { label: "speed",  y: velocidadPokemon},
